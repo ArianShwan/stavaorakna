265358d2c592e344d2510874e4c4e22b3e9c7f1d
Arian Shwan <as228xs@student.lnu.se>
2025-04-24 13:12:50 +0200

    Add the keyboard buttons

--- app.js
+++ app.js
@@ -3,17 +3,13 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 app.set('view engine', 'ejs');
-
 
 app.get('/', (req, res) => {
   res.render('index');
 });
-
 
 app.listen(port, () => {
   console.log(`Stavningsspelet körs på http://localhost:${port}`);
